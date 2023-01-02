900c2e88600dc4a87357cc3c3f03431f27b154b2
mordecai <johnbasilogbhemhe@gmail.com>
2023-01-02 11:34:54 +0100

    create two int variables for the left and right dice, imported the math library used the Random() class to generate numbers from 1-6

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -23,7 +24,7 @@ class DicePage extends StatefulWidget {
 }
 
 class _DicePageState extends State<DicePage> {
-  int leftDiceNumber = 1;
+  int leftDiceNumber = 2;
   int rightDiceNumber = 1;
   @override
   Widget build(BuildContext context) {
@@ -34,14 +35,22 @@ class _DicePageState extends State<DicePage> {
             child: MaterialButton(
                 onPressed: () {
                   setState(() {
-                    leftDiceNumber = 6;
+                    leftDiceNumber = Random().nextInt(6) + 1;
                   });
+
+                  print("diceNumber: $leftDiceNumber");
                 },
                 child: Image.asset('images/dice$leftDiceNumber.png')),
           ),
           Expanded(
             child: MaterialButton(
-                onPressed: null,
+                onPressed: () {
+                  setState(() {
+                    rightDiceNumber = Random().nextInt(6) + 1;
+                  });
+
+                  print("diceNumber: $rightDiceNumber");
+                },
                 child: Image.asset("images/dice$rightDiceNumber.png")),
           )
         ],
